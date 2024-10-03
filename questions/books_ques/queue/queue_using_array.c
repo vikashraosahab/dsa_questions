71bc21c88847299891d7b16fe55506a8211b6087
vikashraosahab <vikashvinayrao@gmail.com>
2024-10-03 13:41:22 +0530

    latested updataion of queue using array

--- questions/books_ques/queue/queue_using_array.c
+++ questions/books_ques/queue/queue_using_array.c
@@ -11,17 +11,56 @@ void initialized (int *,int *);
 void enqueue (int *,int);
 void dequeue (int *);
 int peek (int *);
+int bottom (int *);
 int validInput ();
-
+void print (int *);
 // MAIN FUNCTION OF THE PROGRAM
 int main (int argc, char * argv[])
 {
   initialized (&front,&rear); // CALLING INITIALIZED FUNCTION METHOD
-  enqueue (queue,validInput()); // TAKE VALIDATE INPUT 
-  // printf ("%d is the top of the queue",peek (queue));
-  dequeue (queue);
- // printf ("%d is the top of the queue",peek (queue));
-  return 0; 
+  while (1)
+  {
+    printf ("\n*********** MAIN MENU **************");
+    printf ("\nEnter : 1 for insert new element in the queue!");
+    printf ("\nEnter : 2 for delete element from the queue!");
+    printf ("\nEnter : 3 for check value at the top of the queue !");
+    printf ("\nEnter : 4 for check value at the bottom of the queue!");
+    printf ("\nEnter : 5 for display every element of the queue !");
+    printf ("\nEnter : 6 for exit");
+    printf ("\nEnter value according to the main menu : ");
+    top:switch (validInput ())
+    {
+      case 1:
+       printf ("\nPlease enter value (only integer ) for insertion : ");
+       enqueue (queue,validInput()); // CALLING ENQUEUE METHOD OF THE QUEUE FOR INSERTION NEW ELEMENT IN THE QUEUE
+       printf ("\nValue is inserted now !");
+       break;
+      case 2:
+       printf ("\nDeletion is in processing !");
+       dequeue (queue);
+       printf ("\nProcess completed now !");
+       break;
+      case 3:
+       printf ("\nTop most element of the queue is %d",peek (queue));
+       break;
+      case 4:
+       printf ("\nTop low element of the queue is %d",bottom (queue));
+       break;
+      case 5:
+       printf ("\nAll the elements of the stack are displaye : ");
+       print (queue);
+       break;
+      case 6:
+        goto lable;
+       break;
+      default:
+       printf ("\nTask isn't found in the main menu!");
+       goto top;
+       break;
+    }
+  }
+   lable:printf ("\nProgram is ended now !");
+   return 0; 
 }
 void initialized (int *front,int *rear) // INITIALIZED QUEUE REAR AND FRONT VARIABLE WITH - 1
 {
@@ -29,7 +68,7 @@ void initialized (int *front,int *rear) // INITIALIZED QUEUE REAR AND FRONT VARI
 }
 int validInput () // VALIDATE INTEGER INPUT IF INPUT VALUE NON INTEGER ASK FOR RE-ENTERING THE VALID DATA
 {
-  int val;
+  int val; // VARIABLE DECLARATIONS
  while (1)
  {
   printf ("\nEnter value only integer value  :  ");
@@ -51,7 +90,7 @@ void enqueue (int *queue,int data) // INSERT DATA IN QUEUE AT THE REAR END OF TH
     printf ("\nQueue is fulled now !");
     return;
   }
-  if (front == rear == -1)
+  if (front == -1 && rear <= -1)
   {
     front = rear = 0;
     queue [rear] = data; // INSERT NEW DATA WHEN QUEUE IS EMPTY
@@ -64,19 +103,18 @@ void enqueue (int *queue,int data) // INSERT DATA IN QUEUE AT THE REAR END OF TH
 void dequeue (int *queue) // DEQUEUE PERFORMS DELETION OF THE ELEMENT FROM TEH QUEUE 
 {
   // CHECK UNDERFLOW CONDITION OF THE QUEUE 
-  if (front == -1 && front >= rear) 
+  if (front == -1 || front >= rear) 
   {
     printf ("\nQueue is empty now !");
-    exit (1); // TERMIANTED
+    // exit (1); // TERMIANTED
   }
 
-  int val = queue [front--]; // VALUE FROM THE FRONT IS DELETED NOW 
+  int val = queue [front++]; // VALUE FROM THE FRONT IS DELETED NOW 
   printf ("\n%d value is deleted now !",val);
 }
-/*
-int peek (int *queue)
+int peek (int *queue) // GET THE TOP MOST ELEMENT OF THE QUEUE
 {
-  if (front == -1 && front >= rear)
+  if (front == -1 && front >= rear) // WHEN QUEUE IS EMPTY (UNDERFLOW CONDITION)
   {
     printf ("\nQueue is empty !");
     return -1;
@@ -84,4 +122,23 @@ int peek (int *queue)
   else
    return queue [rear]; // TOP OF THE QUEUE 
 }
-*/
+int bottom (int *queue)
+{
+  if (front == -1 || front >= rear)
+  {
+    printf ("\nQueue is empty !");
+    return -1;
+  }
+  return queue [front];
+}
+void print (int *queue) // DISPLAY EACH CHARACTER FOR THE QUEUE 
+{
+  if (front == -1 && front >= rear) // WHEN QUEUE IS EMPTY (UNDERFLOW CONDITION)
+    printf ("\nQueue is empty now !");
+  else 
+  {
+    int i = front;
+    for (i; i <= rear; i++)
+      printf ("\nElement of the queue is %d",queue [i]);
+  }
+}
