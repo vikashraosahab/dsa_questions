8eb982c72c1f8cd738f420734057f4987d3d362b
vikashraosahab <vikashvinayrao@gmail.com>
2024-10-05 04:03:44 +0530

    updation

--- questions/books_ques/queue/queue_using_array_v2.c
+++ questions/books_ques/queue/queue_using_array_v2.c
@@ -1,111 +1,81 @@
-// PROGRAM OF IMPLEMENTATION OF THE QUEUE USING ARRAY AND STRUCTURE
+// PROGRAM THAT USE QUEUE DATA STRUCTURE TO STORE AND MANIPULATE DATA 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <math.h>
-#include <string.h>
 #include <stdbool.h>
 
-// MACROS DEFINITION FOR THE CONSTANT 
-#define MAXIMUM 5// MAXSIZE FOR MAXIMUM LENGTH OF THE QUEUE
-#define EMPTY -1 // EMPTY CONSTANT FOR EMPTY THE QUEUE
+// MACROS DEFINITION FOR THE CONSTANT DEFINITION
+#define MAX_SIZE 50
+#define EMPTY -1
 
-typedef struct 
-{
-  int queue [MAXIMUM];
-  int front; // FOR DELETION OF THE FIRST ELEMENT FROM THE QUEUE (FIFO)
-  int rear; // FOR INSERTION OF THE NEW ELENENT IN THE QUEUE
-}QUEUE;
-
-// FUNCTIONS PROTOTYPES FOR MODULARITY
-void initialized_queue (QUEUE *); // METHOD THAT INITIALIZED QUEUE'S VARIABBLE FRONT AND REAR AS -1
-bool is_queue_empty (QUEUE *); // METHOD THAT CHECK QUEUE IS EMPTY OR NOT
-bool is_queue_fulled (QUEUE *); // METHOD THAT CHECK QUEUE IS FULLED OR NOT
-void enqueue (QUEUE *,int); // METHOD THAT INSERT NEW ELEMENT IN THE QUEUE 
-void dequeue (QUEUE *); // METHOD THAT DELETE ELEMENT FROM THE QUEUE
-int peek (QUEUE *); // METHOD THAT RETURN TOP MOST ELEMENT OF THE QUEUE THAT PRESENT AT THE REAR
-int bottom (QUEUE *); // METHOD THAT RETURN BOTTOM MOST ELEMENT OF THE QUEUE THAT PRESENT AT THE FRONT
-void display_menu (); // METHOD THAT DISPLAY MENU OF THE TASK
-void menu_task (QUEUE *,int); // METHOD THAT PERFORM EACH TASK OF THE MENU BY USER INPUT
-int valid_input (); // METHOD THAT VALIDATE INPUT FROM USER AND ALLOW ONLY INTEGER AS INPUT AND REFUSE OTHER DATA TYPE LIKE CHARACTER OR STRING , FLOAT ETC
+typedef struct { // DECLARATION OF THE STRUCTURE
+  int queue [MAX_SIZE];
+  int front;
+  int rear;
+} QUEUE;
+// FUNCTION PROTOTYPE 
+void initialize_queue (QUEUE *);
+bool is_queue_empty (QUEUE *);
+bool is_queue_fulled (QUEUE *);
+int size_of_queue (QUEUE *);
+void enqueue (QUEUE *,int);
+int dequeue (QUEUE *);
+void display_menu ();
+void process_menu (QUEUE *,int);
+int peek (QUEUE *);
+int bottom (QUEUE *);
+void display (QUEUE *);
+int valid_input ();
 
 // MAIN FUNCTION OF THE PROGRAM 
-int main (int argc, char * argv[])
-{
-  QUEUE queue; // VARIABLE DECLARATIONS OF THE QUEUE ADT
-  initialized_queue (&queue);
-  while (1)
-  {
-    int val;
-    display_menu (); // SHOWING MENU CARD
-    val = valid_input (); // INPUT STATEMENT BY CALLING VALID_INPUT METHOD
-    if (val == 6) goto finish;
-    menu_task (&queue,val);
+int main (int argc, char * argv[]) {
+  QUEUE queue; // STRUCTURE VARIABLE DECLARATIONS
+  QUEUE *q = &queue; // PASSING ADDRESS OF THE  STRUCTURE VARIABLE IN THE STRUCTURE POINTER 
+  while (1) {
+     display_menu (); // SHOWING MENU BAR TO THE USER
+     value = valid_input (); // TAKE VALIDATE INPUT
+    process_menu (); // PROCESS EACH MENU BY USER INPUT
   }
-  finish:printf ("\nProgram is ended now !");
-  return 0; // TERMINATE THE PROGRAM AFTER SUCCESSFULLY EXECUTION 
+  return 0; // IT TERMINATE THE PROGRAM 
 }
-void initialized_queue (QUEUE *queue) // FUNCTION THAT INITTIALIZED QUEUE ADT
-{
-  queue->front = queue->rear = -1; // INITTIALIZED FRONT AND REAR BY -1
+void initialize_queue (QUEUE *queue) { // FUNCTION THAT INITIALIZED QUEUE FRONT AND REAR AS -1
+
+  queue->front = queue->rear = -1;
 }
-bool is_queue_empty (QUEUE *queue) // CHECK THAT QUEUE IS EMPTY OR NOT 
-{
-  if (queue->front == -1 || queue->front >= queue->rear)
-    return true; // WHEN QUEUE IS EMPTY
-  else 
-    return false;
-}
-bool is_queue_fulled (QUEUE *queue) // CHECK THAT QUEUE IS FULLLED OR NOT
-{
-  if (queue->rear >= MAXIMUM)
-    return true; // WHEN QUEUE IS FULLED 
-  else 
-    return false; // WHEN QUEUE IS EMPTY
-}
-void display_menu () // SHOWING MENU BAR WITH THEIR RESPECTIVE WORKING
-{
-  printf ("\n************** MAIN MENU **************");
-  printf ("\nEnter : 1 for insert new element in the queue!");
-  printf ("\nEnter : 2 for remove element from the queue!");
-  printf ("\nEnter : 3 for getting top most element of the queue!");
-  printf ("\nEnter : 4 for getting bottom most element of the queue!");
-  printf ("\nEnter : 5 for display the element of the queue!");
-  printf ("\nEnter : 6 for exit the program!");
-  printf ("\nEnter the value according to the menu for perform the task : ");
-}
-int valid_input () // TAKE ONLY INTEGER AS INPUT
-{
+int valid_input () { // VALIDATE USER INPUT FOR INTEGER
   int val;
-  while (1)
-  {
-    if (scanf ("%d",&val) != 1) // WHEN VALUE IS NOT INTEGER 
-    {
-      while (getchar () != '\n'); // IF CHARACTER IS NOT NEW LNE 
-       printf ("\nIn-valid input. Pleaes re-enter integer value again : ");
+  printf ("\nPlease enter right value : ");
+  while (1) {
+    if (scanf ("%d",&val) != 1) {  // SCAN USER ENTER RIGHT INPUT OR NOT
+      while (getchar () != '\n')
+       printf ("\nIn-valid input. Please enter valid input. ");
       continue;
     }
-    else  //  WHEN VALUE IS INTEGER 
-      break;
+    else  // STOP THE LOOP WHEN USER ENTER RIGHT INPUT
+     break;
   }
-
   return val;
 }
-void menu_task (QUEUE *queue, int choice) // OPERATIONS ARE PEROFORMED ACCORDING USER INPUT
-{
-  int value;
-  switch (choice) // PERFORM OPERATIONS BAESD ON USER INPUT
-  {
+void display_menu () { // DISPLAY MENU BAR FOR HELP USER TO EXECUTE THIER RESPECTIVE PROGRAM
+  printf ("\n*************** MAIN MENU ********************");
+  printf ("\nEnter : 1 for insertion value in the queue : ");
+  printf ("\nEnter : 2 for deletion value from the queue : ");
+  printf ("\nEnter : 3 for get top most elemenet of the queue : ");
+  printf ("\nEnter : 4 for get bottom most element of the queue : ");
+  printf ("\nEnter : 5 for get total size of the queue : ");
+  printf ("\nEnter : 6 for displaying each value of the queue : ");
+  printf ("\nEnter task according main menu : ");
+}
+void process_menu (QUEUE *queue,int choice) { // PROCESS EACH OPERATIONS BASED ON THE CHOICE OF THE USER
+  switch (choice) {
     case 1:
-      printf ("\nInsert new element in the queue !");
-      value = valid_input (); // TAKE INPUT ONLY INTEGER
-      enqueue (queue,value); 
+      printf ("\nPlease enter valid that you want to insert in the queue : ");
+
       printf ("\nInsertion is done !");
       break;
     case 2:
-      printf ("\nDeletion is in process");
-      dequeue (queue);
-      printf ("\nDeletion is done !");
       break;
     case 3:
       break;
@@ -113,33 +83,7 @@ void menu_task (QUEUE *queue, int choice) // OPERATIONS ARE PEROFORMED ACCORDING
       break;
     case 5:
       break;
+    case 6:
+      break;
   }
 }
-void enqueue (QUEUE *queue, int value) // INSERT NEW VALUE IN THE QUEUE
-{
-  if (is_queue_fulled(queue) == true) // WHEN QUEUE GET FULLLED
-  {
-    printf ("\nQueue is fullled !");
-    return;
-  }
-  if (is_queue_empty (queue) == false) // WHEN QUEUE IS EMPTY 
-  {
-    queue->front = 0;
-    queue->rear = 0;
-  }
-  else  // WHEN QUEUE EITHER FULLED OR EMPTY
-    queue->rear = queue->rear + 1;
-  
-  queue->queue[queue->rear] = value;
-}
-void dequeue (QUEUE *queue) // REMOVE ELEMENT FROM THE QUEUE
-{
-  if (is_queue_empty (queue) == true)
-  {
-    printf ("\nQueue is empty !");
-    return;
-  }
-  int val = queue->queue [queue->front++];
-  printf ("\n%d is deleted now !",val);
-}
-
