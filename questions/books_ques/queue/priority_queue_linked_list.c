a89182b43ee6e2130dba269c69d08c7b9936f45a
vikashraosahab <vikashvinayrao@gmail.com>
2024-10-13 11:24:07 +0530

    priority_queue_linked_chain

---
+++ questions/books_ques/queue/priority_queue_linked_list.c
@@ -0,0 +1,13 @@
+// IMPLEMENTATION OF THE PRIORITY QUEUE BY USING LINKED LIST 
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+
+// MAIN FUNCTION OF THE PROGRAM
+int main (int argc, char * argv[]) {
+ 
+    return 0; // TERMINATION
+}
