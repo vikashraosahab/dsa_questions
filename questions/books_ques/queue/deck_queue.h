a77b0ad3d6b94c48a1e2eeabe0c28a48dd9355a5
vikashraosahab <vikashvinayrao@gmail.com>
2024-10-20 23:01:11 +0530

    updation

--- questions/books_ques/queue/deck_queue.h
+++ questions/books_ques/queue/deck_queue.h
@@ -6,13 +6,22 @@ class DeckQueue {
    //const int max_size = 20;
    DeckQueue (); // CONSTRUCTOR METHOD OF THE CLASS
    ~DeckQueue (); // DESTRUCTOR METHOD OF THE CLASS 
+  void process_menu (const int&);
+  int valid_input ();
+  void menu (const int&);
+  DeckQueue &output_restrict ();
+  DeckQueue &input_restrict ();
+  DeckQueue &insert_end (DeckQueue &);
+  DeckQueue &insert_front (DeckQueue &);
+  DeckQueue &deletion_end (DeckQueue &);
+  DeckQueue &deletion_front (DeckQueue &);
   DeckQueue &display ();
   // PRIVATE MODIFIERS MEMBERS ARE HIGHLY SECURED OR PROTECTED MEMBERS THAT NO OTHER PART OF THE CODE CAN ACCESS EXCEPT IT'S CLASS PUBLIC MEMBERS FUNCTIONS 
   // PRIVATE IS A MODIFIER THAT USED FOR DECLARATIONS OF THE PRIVATE DATA MEMBERS 
   private:
   int array [50];
-  int front;
   int rear;
+  int front;
 }; // SEMI-COLON IS VERY IMPORTANT TO INSERT WHEN CLASS DECLARATIONS IS ENED AFTER CLOSED CURLY BRACKETS 
 
 #endif 
--- questions/books_ques/queue/deck_queue_header.cpp
+++ questions/books_ques/queue/deck_queue_header.cpp
@@ -1,17 +1,117 @@
 // CLASS DEFINITIONS
 #include "deck_queue.h"
 #include <iostream>
-
+#define TASK 1 // SYMBOLIC CONSTANT 
 // CLASS CONSTRUCTOR DEFINITION
-DeckQueue::DeckQueue () {
-    std::cout << "Hello,World\n" << std::endl;
+DeckQueue::DeckQueue ():
+  left_insert (-1),
+  right_insert (-1),
+  right_delete (-1),
+  left_delete (-1) {
+    while (1) {
+      menu (TASK); // SHOW EACH USER PROMPT 
+      int task = valid_input (); // TAKE VALD INPUT
+      if (task == 3) break; // STOP THE LOOP 
+      process_menu (task); // PERFORM TASK ACCORDING TO THE USER INPUT
+    }
 }
 // CLASS PSEUDO MEMBER FUNCTIONS
 DeckQueue& DeckQueue::display () {
-    std::cout << "Hello,World by Vikash Yadav\n" << std::endl;
+    std::cout << "Hello,World by Vikash Yadav\n" << std::endl; // OUTPUT
 
     return *this;
 }
+DeckQueue& DeckQueue::input_restrict () {
+  while (1) {
+   int value;
+   std::cout << "Enter value : " << std::endl;
+   value = valid_input ();
+  switch (value) {
+    case 1:
+      break;
+    case 2:
+      break;
+  
+   }
+  } 
+  return *this;
+}
+DeckQueue& DeckQueue::output_restrict () {
+
+  return *this;
+}
+// FUNCTION THAT DISPLAY ALL MENU PROMPT TO THE USER
+void DeckQueue::menu (const int& task){
+  switch (task) {
+    case 1:
+       std::cout << "Menu : \n";
+       std::cout << "\n1 : Input restriction queue : \n";
+       std::cout << "\n2 : Output restirction queue : \n";
+       std::cout << "\n3 : EXIT \n";
+       std::cout << "\nPlease enter valid input according to the main menu : ";
+      break;
+    case 2:
+       std::cout << "Menu : \n";
+       std::cout << "\n1 :  : \n";
+       std::cout << "\n2 :  : \n";
+       std::cout << "\n3 : EXIT \n";
+       std::cout << "\nPlease enter valid input according to the main menu : ";
+      break;
+  }
+}
+// PROCESS MENU FUNCTION ACTUALLY TAKES INPUT AS PARAMENTER AND PERFORM ACCORDING TASK
+void DeckQueue::process_menu (const int& task) {
+   switch (task) {
+     case 1:
+       std::cout << "Input Restriction queue menu open : \n";
+         
+       std::cout << "\nExisted Now !";
+       break;
+     case 2:
+       std::cout << "\nOutput Restriction queue menu open : \n";
+       
+       std::cout << "\nExisted Now !";
+       break;
+   }
+}
+DeckQueue& DeckQueue::insert_end (DeckQueue& queue) {
+  
+  return *this;
+}
+DeckQueue& DeckQueue::insert_front (DeckQueue& queue) {
+
+  return *this;
+}
+DeckQueue& DeckQueue::deletion_end (DeckQueue& queue) {
+
+  return *this;
+}
+DeckQueue& DeckQueue::deletion_front (DeckQueue& queue) {
+  
+  return *this;
+}
+int DeckQueue::valid_input () {
+  int val;
+  while (1) {
+   // PERFROM EXCEPTION HANDLING BETTER 
+  start:std::cout << "\nPlease enter value : ";
+   try { // TRY BLOCK FIND OUT CACHE EXPRESSION
+     if (scanf ("%d",&val) != 1) { // WHEN VAL IS NOT AN INTEGER VALUE
+      while (getchar () != '\n')
+       throw val; // THEN THROW IT AS THE ERROR 
+      continue;
+    }
+    else // OTHERWISE BREAK THE LOOP
+      break;
+   } // CATCH METHOD SOLVED THE EXECEPTIONS 
+   catch (...) {
+     std::cout << "Error ! Input is in-valid\n Please enter valid input ? " << std::endl; // STATEMENT
+     goto start;
+   }
+  }
+  return val; // RETURN VAL 
+}
+// CLASS MEMBER DESTRUCTOR DEFINITION 
 DeckQueue::~DeckQueue () {
-    std::cout << "Destructor of the class" << std::endl;
+    std::cout << "Program is ended now !" << std::endl; // OUTPUT 
 }
